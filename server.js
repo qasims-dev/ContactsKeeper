8409d9f4691460ec0bdf27ea9eb048d9b8ec7062
qasims-dev <qasims.dev@gmail.com>
2019-08-12 00:02:05 +0300

    Added Register User here

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
+
 const app = express();
 
 //Connect Database
 connectDB();
 
-app.get("/",(req,res)=>{
-    res.json({msg:"Welcome to the react"});
+//Init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the react" });
 });
 
-app.use("/api/users",require("./routes/users"));
-app.use("/api/contacts",require("./routes/contacts"));
-app.use("/api/auth",require("./routes/auth"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log(`Server Running on Port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`));
